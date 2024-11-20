db20eff91a4fa84bf6ff70ea7c8c59835c2b7ce8
hankedan000 <dany32412@gmail.com>
2024-11-20 08:02:59 -0500

    Fixes race condition in BackgroundImgLoader

--- game/scripts/BackgroundImgLoader.gd
+++ game/scripts/BackgroundImgLoader.gd
@@ -31,13 +31,13 @@ func load_texture(filepath: String, callback_func: Callable, is_main_cover: bool
 	_start_next_img_load()
 
 func _start_next_img_load() -> void:
+	_img_load_mutex.lock()
 	if _img_load_request_queue.size() > 0 and _running_img_load_threads < _max_img_load_threads:
 		var next_req := _img_load_request_queue.pop_front() as ImgLoadRequest
 		
-		_img_load_mutex.lock()
 		if next_req and next_req.thread.start(_load_img_threaded.bind(next_req)) == OK:
 			_running_img_load_threads += 1
-		_img_load_mutex.unlock()
+	_img_load_mutex.unlock()
 
 func _load_img_threaded(req: ImgLoadRequest) -> void:
 	# read cover image data from file into a buffer
